5d6c92e61ae583092ac5a5fc6bce042ea46aaad9
Han <hanbomb@hotmail.com>
2017-04-09 02:04:55 +0800

    1.0

---
+++ src/GenerateRandomNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class GenerateRandomNumbers {
+	
+	public static void main(String args[])
+	{
+		System.out.println("How many numbers to generate?");
+		Scanner input=new Scanner(System.in);
+		int RandomNumCount=input.nextInt();
+		
+		System.out.println("What's the radium number?");
+		int radius=input.nextInt();
+		int diameter=radius*2;
+		
+		int num[]=new int[RandomNumCount];
+		for(int i=0;i<RandomNumCount;i++)
+		{
+			num[i]=(int)(Math.random()*diameter);
+			System.out.print(num[i]+ " ");
+		}
+	}
+}
